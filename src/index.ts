ae8edf26e1664106d9ab1961e7bba22df6dd10d4
Wesley Leal <wslleall@gmail.com>
2023-02-13 04:47:24 -0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,86 @@
 import bodyParser from "body-parser";
 import express from "express";
-import pg from "pg";
-
-// Connect to the database using the DATABASE_URL environment
-//   variable injected by Railway
-const pool = new pg.Pool();
+const qrcode = require('qrcode');
+const { Client, NoAuth } = require('whatsapp-web.js');
 
 const app = express();
-const port = process.env.PORT || 3333;
+app.set('view engine', 'ejs');
+const port = process.env.PORT || 3331;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+const client = new Client({
+    authStrategy: new NoAuth(),
+    puppeteer: {
+        headless: true,
+		args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', // <- this one doesn't works in Windows
+            '--disable-gpu'
+        ],
+	}
+});
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+// app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+  
+  res.send(`Hello, World!`);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.get('/zap', (req, res) => {
+    console.log('zap');
+
+    client.on('qr', (qr: any) => {
+        console.log('QR RECEIVED');
+        qrcode.toDataURL(qr, (err: any, url: any) => {
+        let html =  `
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Hello!</title>
+                <style>
+                    .centered{
+                        background: #000;
+                        width: 100vw;
+                        height: 100vh;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="centered">
+                    <img width="300" src="${url}" />
+                </div>
+            </body>
+            </html>
+            `
+            res.type('html').send(html)
+        });
+      });
+})
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', (message:any) => {
+    if(message.body === '#ping') {
+        message.reply('pong');
+    }
+});
+ 
+
+client.initialize();
+ 
